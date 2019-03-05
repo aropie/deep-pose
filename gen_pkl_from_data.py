8c753ded064471800b5bbb6130a75c669691160e
Adrian Rodriguez <aropie@hotmail.com>
2019-03-05 12:29:57 -0600

    Further cleaning

--- gen_pkl_from_data.py
+++ gen_pkl_from_data.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 import sys
-sys.path.append("/home/marciniega/Things_Dock/Proyecto/clean_start/pdbmani_adrian/pdb_manipulation")
-sys.path.append("/home/marciniega/Things_Dock/Proyecto/clean_start/pdbmani_adrian/math_tricks")
 from pdbqt import Pdbqt, PdbqtRes
-from read_pdb_tools import Pdb
 import cPickle as pickle
 import math_vect_tools
 import os
+sys.path.append("/home/marciniega/Things_Dock/Proyecto/clean_start/pdbmani_adrian/pdb_manipulation")
+sys.path.append("/home/marciniega/Things_Dock/Proyecto/clean_start/pdbmani_adrian/math_tricks")
 
 WORK_DIR = "/home/marciniega/Things_Dock/Proyecto/clean_start"
 POSES_DIR = WORK_DIR+"/poses"
 DOCK_DIR = WORK_DIR+"/docked_data"
 REF_DIR = WORK_DIR+"/reference_data"
 OBJ_DIR = WORK_DIR+"/working_objects"
+
 
 def prepareBranches(lig, res):
     """
@@ -22,18 +22,23 @@ def prepareBranches(lig, res):
         bl_center = bl.getGeometricCenter()
         for r in res.residues.values():
             for br in r.branches:
-                if len(br.atoms) == 0: 
-                   continue
+                if len(br.atoms) == 0:
+                    continue
                 distance = math_vect_tools.distance(bl_center, br.getGeometricCenter())
                 if distance <= RNG:
                     bl.close_branches.append((br, distance))
                     br.close_branches.append((bl, distance))
 
+
 def detachPoses(lig_file):
     """
-    Recives a docked ligand file a list of tuples (pose, score)
-    where pose is a pdbqt object containing only the pose and
-    score is the score asociated to that pose
+    Recieves a docking result file and divides it into pdb object
+    per pose
+
+    :param lig_file: docking result file
+    :return: [(pose1, score1), (pose2, score2), ...]
+    :rtype: (Pdbqt, int)[]
+
     """
     pose_num = 1
     poses = []
@@ -57,6 +62,7 @@ def detachPoses(lig_file):
             f.write(line)
     return poses
 
+
 def fileLen(fname):
     """
     Returns the number of lines in a file
@@ -66,6 +72,7 @@ def fileLen(fname):
             pass
     return i + 1
 
+
 if __name__ == "__main__":
 
     RNG = 6
@@ -73,18 +80,17 @@ if __name__ == "__main__":
     DICTIONARY = pickle.load(open(OBJ_DIR+"/resi_branch_dict.pkl", "rb"))
 
     list_of_docked = sys.argv[1]
-    
-    for f in open(list_of_docked,'r').readlines():
+
+    for f in open(list_of_docked, "r").readlines():
         try:
-            f = f.split('\n')[0]
+            f = f.split("\n")[0]
             file_name = f[:-6]
-            dir_name = POSES_DIR +'/'+ file_name
+            dir_name = POSES_DIR + "/" + file_name
             os.mkdir(dir_name)
-            docked_file = open(DOCKED_DIR + "/" + f , 'rb')
-            ref_lig = Pdbqt(REF_DIR+"/ligands_pdbqt/L_" + f)
-            res = PdbqtRes(REF_DIR+"/residues_pdbqt/R_" + f, DICTIONARY)
-            poses = detachPoses(docked_file)
-            docked_file.close()
+            with open(os.path.join(DOCK_DIR, f), "rb") as docked_file:
+                ref_lig = Pdbqt(REF_DIR+"/ligands_pdbqt/L_" + f)
+                res = PdbqtRes(REF_DIR+"/residues_pdbqt/R_" + f, DICTIONARY)
+                poses = detachPoses(docked_file)
             i = 1
             for (pose, score) in poses:
                 prepareBranches(pose, res)
@@ -99,8 +105,3 @@ if __name__ == "__main__":
         except Exception, err:
             sys.stderr.write(file_name + "   ")
             sys.stderr.write(str(err) + "\n")
-            # Hasta aqui, ya dividimos al archivo de docking en un objeto pdbqt para
-            # cada pose. La lista 'poses' tiene tuplas con el objeto pdbqt de la pose
-            # y su score. Falta calcular el rmsdi de cada pose con respecto a la original
-            # y hacer el archivo de cada pose con los pares. Falta igual acomodar los pares
-            # en un formato de columnas.
