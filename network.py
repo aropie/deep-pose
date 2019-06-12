492009a18894d69fb1e4a363c543da6e8e02b690
Adrian Rodriguez <aropie@hotmail.com>
2019-06-12 13:41:53 -0500

    Extract training into a function

--- network.py
+++ network.py
@@ -28,45 +28,6 @@ def add_zeros(sample):
     return np.array(sample, dtype="int32"), np.array(s, dtype="int32")
 
 
-print("Reading dataset")
-with open(sys.argv[1], "rb") as f:
-    dataset = pkl.load(f)
-print("Loaded {} samples in dataset".format(len(dataset)))
-train_perc = 0.8
-test_perc = 0.1
-valid_perc = 0.1
-
-total = len(dataset)
-train_index = int(total*train_perc)
-test_index = int(train_index + total*test_perc)
-valid_index = int(test_index + total*valid_perc)
-
-train_set = dataset[:train_index]
-test_set = dataset[train_index:test_index]
-valid_set = dataset[test_index:]
-
-print("{}, {}, {} samples in train, test and validation datasets respectively ({}%, {}%, {}%)"
-      .format(len(train_set), len(test_set), len(valid_set), train_perc*100, test_perc*100, valid_perc*100))
-train_set_x, train_set_y = zip(*train_set)
-test_set_x, test_set_y = zip(*test_set)
-valid_set_x, valid_set_y = zip(*valid_set)
-
-test_set_x,  s_t = add_zeros(test_set_x)
-test_set_x = theano.shared(test_set_x)
-s_t = theano.shared(s_t, borrow=True)
-test_set_y = theano.shared(np.asarray(test_set_y, dtype="int32"))
-
-train_set_x, s_s = add_zeros(train_set_x)
-train_set_x = theano.shared(train_set_x)
-s_s = theano.shared(s_s, borrow=True)
-train_set_y = theano.shared(np.asarray(train_set_y, dtype="int32"))
-
-valid_set_x, s_v = add_zeros(valid_set_x)
-valid_set_x = theano.shared(valid_set_x)
-s_v = theano.shared(s_v, borrow=True)
-valid_set_y = theano.shared(np.asarray(valid_set_y, dtype="int32"))
-
-
 class LogisticRegression(object):
     def __init__(self, input, n_in, n_out):
         self.W = theano.shared(
@@ -280,142 +241,188 @@ def save_model_naive(model, epoch):
         cPickle.dump(model, f, protocol=cPickle.HIGHEST_PROTOCOL)
 
 
-batch_size = 50
-n_train_batches = train_set_x.get_value(borrow=True).shape[0]
-n_valid_batches = valid_set_x.get_value(borrow=True).shape[0]
-n_test_batches = test_set_x.get_value(borrow=True).shape[0]
-n_train_batches //= batch_size
-n_valid_batches //= batch_size
-n_test_batches //= batch_size
+def main(train=True):
+    print("Reading dataset")
+    with open(sys.argv[1], "rb") as f:
+        dataset = pkl.load(f)
+    print("Loaded {} samples in dataset".format(len(dataset)))
+    train_perc = 0.8
+    test_perc = 0.1
+    valid_perc = 0.1
+
+    total = len(dataset)
+    train_index = int(total*train_perc)
+    test_index = int(train_index + total*test_perc)
+    valid_index = int(test_index + total*valid_perc)
+
+    train_set = dataset[:train_index]
+    test_set = dataset[train_index:test_index]
+    valid_set = dataset[test_index:]
+
+    print("{}, {}, {} samples in train, test and validation datasets respectively ({}%, {}%, {}%)"
+        .format(len(train_set), len(test_set), len(valid_set), train_perc*100, test_perc*100, valid_perc*100))
+    train_set_x, train_set_y = zip(*train_set)
+    test_set_x, test_set_y = zip(*test_set)
+    valid_set_x, valid_set_y = zip(*valid_set)
+
+    test_set_x,  s_t = add_zeros(test_set_x)
+    test_set_x = theano.shared(test_set_x)
+    s_t = theano.shared(s_t, borrow=True)
+    test_set_y = theano.shared(np.asarray(test_set_y, dtype="int32"))
+
+    train_set_x, s_s = add_zeros(train_set_x)
+    train_set_x = theano.shared(train_set_x)
+    s_s = theano.shared(s_s, borrow=True)
+    train_set_y = theano.shared(np.asarray(train_set_y, dtype="int32"))
+
+    valid_set_x, s_v = add_zeros(valid_set_x)
+    valid_set_x = theano.shared(valid_set_x)
+    s_v = theano.shared(s_v, borrow=True)
+    valid_set_y = theano.shared(np.asarray(valid_set_y, dtype="int32"))
 
 
-z = tt.ivector()
-x = tt.itensor4()
-y = tt.ivector()
-index = tt.iscalar()
 
-BRANCHES_IN_DICT = 4472+1
-HYPER_NEIGHBORS = 80
-HYPER_CF = 150
-HIDDEN_1 = 50
-HIDDEN_2 = 20
-OUT = 2
-
-layer_0 = DataTreatmentLayer(rng, x, z, HYPER_NEIGHBORS, BRANCHES_IN_DICT)
-layer_1 = ConvLayer(rng, layer_0.output, layer_0.cut_positions,
-                    HYPER_NEIGHBORS * 10, HYPER_CF)
-layer_2 = HiddenLayer(rng, layer_1.output, HYPER_CF, HIDDEN_1)
-layer_3 = HiddenLayer(rng, layer_2.output, HIDDEN_1, HIDDEN_2)
-layer_out = LogisticRegression(layer_3.output, HIDDEN_2, OUT)
-
-layers = [layer_0, layer_1, layer_2, layer_3, layer_out]
-
-cost = layer_out.negative_log_likelihood(y)
+    batch_size = 50
+    n_train_batches = train_set_x.get_value(borrow=True).shape[0]
+    n_valid_batches = valid_set_x.get_value(borrow=True).shape[0]
+    n_test_batches = test_set_x.get_value(borrow=True).shape[0]
+    n_train_batches //= batch_size
+    n_valid_batches //= batch_size
+    n_test_batches //= batch_size
 
 
-test_model = theano.function(
-    [index],
-    layer_out.errors(y),
-    givens={
-        x: test_set_x[index*batch_size:(index+1)*batch_size],
-        y: test_set_y[index*batch_size:(index+1)*batch_size],
-        z: s_t[index*batch_size:(index+1)*batch_size]
-    }
-)
+    z = tt.ivector()
+    x = tt.itensor4()
+    y = tt.ivector()
+    index = tt.iscalar()
 
-validate_model = theano.function(
-    [index],
-    layer_out.errors(y),
-    givens={
-        x: valid_set_x[index*batch_size:(index+1)*batch_size],
-        y: valid_set_y[index*batch_size:(index+1)*batch_size],
-        z: s_v[index*batch_size:(index+1)*batch_size]
-    }
-)
+    BRANCHES_IN_DICT = 4472+1
+    HYPER_NEIGHBORS = 80
+    HYPER_CF = 150
+    HIDDEN_1 = 50
+    HIDDEN_2 = 20
+    OUT = 2
 
-params = layer_out.params + layer_3.params + layer_2.params + layer_1.params + layer_0.params
+    layer_0 = DataTreatmentLayer(rng, x, z, HYPER_NEIGHBORS, BRANCHES_IN_DICT)
+    layer_1 = ConvLayer(rng, layer_0.output, layer_0.cut_positions,
+                        HYPER_NEIGHBORS * 10, HYPER_CF)
+    layer_2 = HiddenLayer(rng, layer_1.output, HYPER_CF, HIDDEN_1)
+    layer_3 = HiddenLayer(rng, layer_2.output, HIDDEN_1, HIDDEN_2)
+    layer_out = LogisticRegression(layer_3.output, HIDDEN_2, OUT)
 
-grads = tt.grad(cost, params)
+    layers = [layer_0, layer_1, layer_2, layer_3, layer_out]
 
-learning_rate = 0.05
-updates = [
-    (param_i, param_i - learning_rate * grad_i)
-    for param_i, grad_i in zip(params, grads)
-]
+    cost = layer_out.negative_log_likelihood(y)
 
-train_model = theano.function(
-    [index],
-    cost,
-    updates=updates,
-    givens={
-        x: train_set_x[index*batch_size:(index+1)*batch_size],
-        y: train_set_y[index*batch_size:(index+1)*batch_size],
-        z: s_s[index*batch_size:(index+1)*batch_size]
-    }
-)
 
-epoch = 0
-done_looping = False
-n_epochs = 1000
-best_validation_loss = np.inf
-best_iter = 0
-test_score = 0.0
+    test_model = theano.function(
+        [index],
+        layer_out.errors(y),
+        givens={
+            x: test_set_x[index*batch_size:(index+1)*batch_size],
+            y: test_set_y[index*batch_size:(index+1)*batch_size],
+            z: s_t[index*batch_size:(index+1)*batch_size]
+        }
+    )
 
-patience = 10000
-patience_increase = 2
-improvement_threshold = 0.995
+    validate_model = theano.function(
+        [index],
+        layer_out.errors(y),
+        givens={
+            x: valid_set_x[index*batch_size:(index+1)*batch_size],
+            y: valid_set_y[index*batch_size:(index+1)*batch_size],
+            z: s_v[index*batch_size:(index+1)*batch_size]
+        }
+    )
 
-validation_frequency = n_train_batches
+    params = layer_out.params + layer_3.params + layer_2.params + layer_1.params + layer_0.params
 
-start_time = timeit.default_timer()
-while (epoch < n_epochs) and (not done_looping):
-    epoch = epoch + 1
-    for minibatch_index in range(n_train_batches):
+    grads = tt.grad(cost, params)
 
-        iter = (epoch - 1) * n_train_batches + minibatch_index
+    learning_rate = 0.05
+    updates = [
+        (param_i, param_i - learning_rate * grad_i)
+        for param_i, grad_i in zip(params, grads)
+    ]
 
-        if iter % 100 == 0:
-            print('training @ iter = ', iter)
-        cost_ij = train_model(minibatch_index)
+    train_model = theano.function(
+        [index],
+        cost,
+        updates=updates,
+        givens={
+            x: train_set_x[index*batch_size:(index+1)*batch_size],
+            y: train_set_y[index*batch_size:(index+1)*batch_size],
+            z: s_s[index*batch_size:(index+1)*batch_size]
+        }
+    )
 
-        if (iter + 1) % validation_frequency == 0:
-            # compute zero-one loss on validation set
-            validation_losses = [validate_model(i)
-                                 for i in range(n_valid_batches)]
-            this_validation_loss = np.mean(validation_losses)
+    start_time = timeit.default_timer()
+    if train:
+        epoch = 0
+        done_looping = False
+        n_epochs = 1000
+        best_validation_loss = np.inf
+        best_iter = 0
+        test_score = 0.0
 
-            print('epoch {}, minibatch {}/{}, validation error {} %'
-                  .format(epoch, minibatch_index + 1, n_train_batches,
-                          this_validation_loss * 100))
-            save_model_good(layers, epoch)
-            # if we got the best validation score until now
-            if this_validation_loss < best_validation_loss:
+        patience = 10000
+        patience_increase = 2
+        improvement_threshold = 0.995
 
-                # improve patience if loss improvement is good enough
-                if this_validation_loss < (best_validation_loss
-                                           * improvement_threshold):
-                    patience = max(patience, iter * patience_increase)
+        validation_frequency = n_train_batches
 
-                    # save best validation score and iteration number
-                    best_validation_loss = this_validation_loss
-                    best_iter = iter
+        while (epoch < n_epochs) and (not done_looping):
+            epoch = epoch + 1
+            for minibatch_index in range(n_train_batches):
 
-                    # test it on the test set
-                    test_losses = [test_model(i)
-                                   for i in range(n_test_batches)]
-                    test_score = np.mean(test_losses)
-                    print(('   epoch {}, minibatch {}/{}, test error of best model {} %')
-                          .format(epoch, minibatch_index + 1, n_train_batches,
-                                  test_score * 100.))
-        if iter >= patience:
-            print("Breaking to prevent overfitting")
-            done_looping = True
-            break
+                iter = (epoch - 1) * n_train_batches + minibatch_index
 
-end_time = timeit.default_timer()
-print('Optimization complete.')
-print('Best validation score of {} % obtained at iteration {}, with test performance {} %'
-      .format(best_validation_loss * 100., best_iter + 1, test_score * 100.))
-print('The code ' + ' ran for %.2fm' % ((end_time - start_time) / 60.))
+                if iter % 100 == 0:
+                    print('training @ iter = ', iter)
+                cost_ij = train_model(minibatch_index)
 
-sys.exit()
+                if (iter + 1) % validation_frequency == 0:
+                    # compute zero-one loss on validation set
+                    validation_losses = [validate_model(i)
+                                         for i in range(n_valid_batches)]
+                    this_validation_loss = np.mean(validation_losses)
+
+                    print('epoch {}, minibatch {}/{}, validation error {} %'
+                        .format(epoch, minibatch_index + 1, n_train_batches,
+                                this_validation_loss * 100))
+                    save_model_good(layers, epoch)
+                    # if we got the best validation score until now
+                    if this_validation_loss < best_validation_loss:
+
+                        # improve patience if loss improvement is good enough
+                        if this_validation_loss < (best_validation_loss
+                                                   * improvement_threshold):
+                            patience = max(patience, iter * patience_increase)
+
+                            # save best validation score and iteration number
+                            best_validation_loss = this_validation_loss
+                            best_iter = iter
+
+                            # test it on the test set
+                            test_losses = [test_model(i)
+                                        for i in range(n_test_batches)]
+                            test_score = np.mean(test_losses)
+                            print(('   epoch {}, minibatch {}/{}, test error of best model {} %')
+                                .format(epoch, minibatch_index + 1, n_train_batches,
+                                        test_score * 100.))
+                if iter >= patience:
+                    print("Breaking to prevent overfitting")
+                    done_looping = True
+                    break
+
+        end_time = timeit.default_timer()
+        print('Optimization complete.')
+        print('Best validation score of {} % obtained at iteration {}, with test performance {} %'
+            .format(best_validation_loss * 100., best_iter + 1, test_score * 100.))
+    print('The code ' + ' ran for %.2fm' % ((end_time - start_time) / 60.))
+
+    sys.exit()
+
+
+if __name__ == '__main__':
+    main()
